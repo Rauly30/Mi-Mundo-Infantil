99156cc735fd374e3d0726a4e802c04c631ecf6f
unknown <Juan Diego@OFP-SIS-PATS.taher.com>
2025-02-21 08:40:14 -0400

    cambio de estructuras 2

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profesor/',views.viepanel_profesor, name='panel_profesor'),
+    path('profesor/',views.panel_profesor, name='panel_profesor'),
     path('administrador/', views.panel_administrador, name='panel_administrador'),
     path('director/', views.panel_director, name='panel_director'),
 
