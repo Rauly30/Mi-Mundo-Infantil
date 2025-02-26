54ee0702026577bc6bc72cb7e4397546b707492b
Raulymar <rau30ly@gmail.com>
2025-02-26 14:08:15 -0400

    cambios style

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('panel_profesorr/',views.panel_profesor, name='panel_profesor'),
+    path('panel_profesor/',views.panel_profesor, name='panel_profesor'),
     path('panel_administrador/', views.panel_administrador, name='panel_administrador'),
     path('panel_director/', views.panel_director, name='panel_director'),
     path('administrador_login/', views.administrador_login, name='administrador_login'),
